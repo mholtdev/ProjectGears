4bd208116276dff1a3c969a63e2c3ee3d49d0ec8
Adrian Dev <adrianjavorski@gmail.com>
2017-11-16 13:10:04 -0800

    Nation's Package
    So its just the creation of a nation generator sort of

---
+++ src/nationsinfo/NationSetup.java
@@ -0,0 +1,17 @@
+package nationsinfo;
+
+public class NationSetup {
+/*So Matt, what I was thinking is that this will be the main Nation class which we will use to build psuedo-random nations
+ * to interact with in the game.
+ */
+/** TO-DO LIST**/
+/* -Create a random name generator for the name of the nation, ruler's name, people's name, currency name,etc.
+ * -Create values and basic traits for the nations goals, provide a initial currency value, figure out how to balance currency values between each other
+ * -Set up that The nations won't overpower the player right at the start of the game, and that if the player doesn't do anything, the world still simualates around him
+ * 
+ */
+	public static void main(String[] args) {
+
+	}
+
+}
